60cc4203868db2d81c6722dda31fc6397bde40b1
kgovind12 <kgovind@ucdavis.edu>
2022-02-04 19:59:14 -0800

    tile puzzle working with numbers

--- script.js
+++ script.js
@@ -4,27 +4,73 @@
     const tiles = document.querySelectorAll('.square');
     const overlay = document.querySelector('.overlay');
     const gameOverMessage = document.querySelector('.game-over-message');
+    const scrambleBtn = document.getElementById('scramble');
+    const solveBtn = document.getElementById('solve');
 
+    let positions = [0, 150, 300, 450];
+    let tileWidth = positions[1];
+    
     const gameMap = {
         "x-1-1": [0,0],
-        "x-1-2": [0,200],
-        "x-1-3": [0,400],
-        "x-1-4": [0,600],
-        "x-2-1": [200,0],
-        "x-2-2": [200,200],
-        "x-2-3": [200,400],
-        "x-2-4": [200,600],
-        "x-3-1": [400,0],
-        "x-3-2": [400,200],
-        "x-3-3": [400,400],
-        "x-3-4": [400,600],
-        "x-4-1": [600,0],
-        "x-4-2": [600,200],
-        "x-4-3": [600,400],
-        "x-4-4": [600,600],
+        "x-1-2": [0, positions[1]],
+        "x-1-3": [0, positions[2]],
+        "x-1-4": [0, positions[3]],
+        "x-2-1": [positions[1], 0],
+        "x-2-2": [positions[1], positions[1]],
+        "x-2-3": [positions[1], positions[2]],
+        "x-2-4": [positions[1], positions[3]],
+        "x-3-1": [positions[2], 0],
+        "x-3-2": [positions[2], positions[1]],
+        "x-3-3": [positions[2], positions[2]],
+        "x-3-4": [positions[2], positions[3]],
+        "x-4-1": [positions[3], 0],
+        "x-4-2": [positions[3], positions[1]],
+        "x-4-3": [positions[3], positions[2]],
+        "x-4-4": [positions[3], positions[3]],
+    }
+
+    scramble();
+
+    scrambleBtn.addEventListener('click', scramble);
+    solveBtn.addEventListener('click', solve);
+
+    function scramble() {
+        // Scramble the array of positions
+        let currentIndex = positions.length;
+        let randomIndex = 0;
+
+        while (currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [positions[currentIndex], positions[randomIndex]] = [positions[randomIndex], positions[currentIndex]];
+        }
+
+        // Display the scrambled tiles
+        let i = 0, j = 0;
+        for (let k = 0; k < tiles.length; k++) {
+            tiles[k].style.top = positions[i] + 'px';
+            tiles[k].style.left = positions[j] + 'px';
+            i++;
+
+            if (i === 4) {
+                i = 0;
+            }
+            if ((k + 1) % 4 === 0) {
+                j++;
+            }
+        }
+    }
+
+    function solve() {
+        for (let tile of tiles) {
+            tile.style.top = gameMap[tile.id][0] + 'px';
+            tile.style.left = gameMap[tile.id][1] + 'px';
+        }
     }
 
     tiles.forEach(function(selectedTile) {
+        // Handle click of any tile
         selectedTile.addEventListener('click', function() {
             const emptyTile = document.querySelector('.empty');
    
@@ -41,7 +87,7 @@
         let selectedTop = tileStyles.getPropertyValue('top').slice(0, -2);
         let selectedLeft = tileStyles.getPropertyValue('left').slice(0, -2);
 
-        if (Math.abs(parseInt(emptyTop) - parseInt(selectedTop)) === 200 && parseInt(emptyLeft) === parseInt(selectedLeft)) {
+        if (Math.abs(parseInt(emptyTop) - parseInt(selectedTop)) === tileWidth && parseInt(emptyLeft) === parseInt(selectedLeft)) {
             // selected tile is swappable
             // swap it
             let temp = selectedTop + 'px';
@@ -52,7 +98,7 @@
                 gameWon();
             }
 
-        } else if (Math.abs(parseInt(emptyLeft) - parseInt(selectedLeft)) === 200 && parseInt(emptyTop) === parseInt(selectedTop)) {
+        } else if (Math.abs(parseInt(emptyLeft) - parseInt(selectedLeft)) === tileWidth && parseInt(emptyTop) === parseInt(selectedTop)) {
             let temp = selectedLeft + 'px';
             selectedTile.style.left = emptyLeft + 'px';
             emptyTile.style.left = temp;
@@ -64,7 +110,6 @@
     }
 
     function gameWon() {
-
         let win = true;
 
         for (let tile of tiles) {
